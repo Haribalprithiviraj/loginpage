12804f3957afcdba39709967d24e53234788be6f
Haribalprithiviraj <haribalprithiviraj18@gmail.com>
2025-05-28 19:17:31 +0530

    fourth update to appear alert

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './login.css';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
+import './components/alert';
 
 function App() {
   return (
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,32 +1,21 @@
 import React from "react"
+import { handleloginclick } from "./alert"
 function Login(){
     return(
-        <div class="container">
-         <h1 class="logo">Login</h1>
+        <div id="container">
+         <h1 id="logo">Login</h1>
          <form method="get">
-            <table>
-                <tr>
-                    <td>
-                               <label id="label1">E-mail id or username</label> 
-                    </td>
-                    <td>
+          
+                               <label id="label1">E-mail id or username</label>  
+                               
                                <input title="E-mailid" id="email" placeholder="Enter E-mail id"  required />
-                    </td>
-                </tr>
-                <tr>
-                    <td>
+                    
                                <label id="label2">Password</label> 
-                    </td>
-                    <td>
+                   
                               <input type="password" id="password" title="password" placeholder="Enter password"  required />
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                            <button class="login" onClick="popup()" title="login">Login</button>
-                    </td>
-                </tr>
-            </table>
+                    
+                            <button id="login" onClick={handleloginclick()} title="login">Login</button>
+                    
          </form>
          </div>
     )
